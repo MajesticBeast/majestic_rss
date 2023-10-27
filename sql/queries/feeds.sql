711a2a56df4bf6f31e5617592d369910f4d043ac
majesticbeast <eric.d.barnard@outlook.com>
2023-10-26 18:10:33 -0700

    add: MarkFeedFetched()

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -10,3 +10,8 @@ SELECT * FROM feeds;
 SELECT * FROM feeds
 ORDER BY last_fetched_at ASC
 LIMIT $1;
+
+-- name: MarkFeedFetched :exec
+UPDATE feeds
+SET last_fetched_at = $1, updated_at = $1
+WHERE id = $2;
